a927ffb628eb939352b41f0f59af8c70c6111e53
mohammadGhiasvand <m.ghiasvand.work@gmail.com>
2024-07-10 23:14:48 +0200

    Changing the logo name and icon

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PiggyBank } from "lucide-react";
+import { Landmark, PiggyBank, Wallet } from "lucide-react";
 import Link from "next/link";
 
 const Logo = () => {
@@ -10,9 +10,9 @@ const Logo = () => {
       className="flex items-center gap-2
   "
     >
-      <PiggyBank className="stroke w-11 h-11 stroke-custom-purple stroke-[1.5]" />
+      <Wallet className="stroke w-11 h-11 stroke-custom-purple stroke-[1.5]" />
       <p className="bg-gradient-to-r from-custom-purple via-custom-red to-custom-orange bg-clip-text text-3xl leading-tight tracking-tighter text-transparent font-medium">
-        BudgetTracker
+        BudgetAnalysis
       </p>
     </Link>
   );
